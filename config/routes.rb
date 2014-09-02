36201b59b718e5914e16ecc8cf22bf06a41c1e75
Rani Itani <itani.rani@gmail.com>
2014-09-02 16:00:16 -0400

    added logic to display login and sign out in application.view

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
  
-  # get 'sessions/new'
-
-  # get 'sessions/create'
-
-  # get 'sessions/destroy'
-
   resources :products
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
