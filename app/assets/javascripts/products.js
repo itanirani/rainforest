c09031cb6598d64b434d656d51dce964cd3d6b02
Rani Itani <itani.rani@gmail.com>
2014-09-16 16:40:25 -0400

    added .ajax

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,20 +1,14 @@
-$(document).on('ready page:load', function(){
-function display_search_results(){
-		if (this.readyState === 4){
-			console.log(this);
-			document.getElementById('products').innerHTML = this.responseText;
-		}
-	}
+$(document).on('ready page:load', function() {
+  $('#search-form').submit(function(event) {
+    event.preventDefault();
+    var searchValue = $('#search').val();
 
-	var form = document.getElementById('search-form');
-	form.addEventListener('submit', function(event){
-		event.preventDefault();
-		var searchValue = document.getElementById('search').value;
-
-		var xhr = new XMLHttpRequest();
-		xhr.onload = display_search_results;
-		xhr.open('GET', '/products?search=' + searchValue, true);
-		xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest")
-		xhr.send();
-	});
+    $.ajax({
+      url: '/products?search=' + searchValue,
+      type: 'GET',
+      dataType: 'html'
+    }).done(function(data){
+      $('#products').html(data);
+    });
+  });
 });
